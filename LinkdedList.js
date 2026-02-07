079a02a43d77261d7831950e417ad17461e222a0
Nagender211 <dnagender2019@gmail.com>
2026-02-07 19:44:51 +0530

    pop method

--- LinkdedList.js
+++ LinkdedList.js
@@ -12,7 +12,58 @@ class LinkdedList{
         this.tail=newNode
         this.length=1
     }
+    push(value){
+        const newNode=new Node(value)
+        if(!this.head){
+            this.head=newNode
+            this.tail=newNode
+        }
+        else{
+            this.tail.next=newNode
+            this.tail=newNode
+        }
+        this.length++
+        return this
+    }
+    pop(){
+        if(!this.head) return undefined
+        let temp=this.head
+        let prev=this.head
+        while(temp.next){
+            prev=temp
+            temp=temp.next
+        }
+        this.tail=prev
+        this.tail.next=null
+        this.length--
+        if(this.length===0){
+            this.head=null
+            this.tail=null
+        }
+        console.log("temp var",temp)
+        console.log("prev ele",prev)
+        return this
+    }
+    
     
 }
-const myLinkdeList=new LinkdedList(6)
+const myLinkdeList=new LinkdedList(5)
 console.log(myLinkdeList)
+console.log(myLinkdeList.push(3))
+console.log(myLinkdeList.push(4))
+console.log(myLinkdeList.push(6))
+console.log(myLinkdeList.pop())
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
